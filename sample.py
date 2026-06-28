60de41d14517d49b57d82cfdb90e3244a2778398
banahattidinesh <banahattidinesh@gmail.com>
2026-06-28 21:11:35 +0530

    created python project pod pipeline and added another file as training

---
+++ sample.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+
+# 1. EXTRACT (Going back to pulling all carts so we have a real table)
+url = "https://dummyjson.com/carts"
+data = requests.get(url).json()
+
+# 2. CLEAN (Convert the list of carts into a pandas DataFrame)
+df = pd.DataFrame(data['carts'])
+
+# Print the first 5 rows of our new table
+print("Raw Table Data:")    
+print(df.head(5))
