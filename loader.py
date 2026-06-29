4a3162fd56c3f32e26d66c7daf83cf47cac01c87
banahattidinesh <banahattidinesh@gmail.com>
2026-06-29 09:01:54 +0530

    refactor: update database connection to use environment variable for password

--- loader.py
+++ loader.py
@@ -1,5 +1,8 @@
+import os
+
 import requests
 import pandas as pd
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 
 url = "https://dummyjson.com/carts"
@@ -7,9 +10,18 @@ data = requests.get(url).json()
 df = pd.DataFrame(data['carts'])
 df = df[['id', 'userId', 'totalProducts']]
 
+
+load_dotenv()
+password = os.getenv("DB_PASSWORD") 
+
+
+
 # YOUR CHALLENGE STARTS HERE!
 
-engine = create_engine('postgresql://postgres:dinesh123@localhost:5433/dataengg_db')
+engine = create_engine(f'postgresql://postgres:{password}@localhost:5433/dataengg_db')
+
+print(f"Success connecting to the database! with password starting with {password[:2]}")
+
 
 df.to_sql('cleaned_pod_orders', engine, if_exists='replace', index=False)  
 
